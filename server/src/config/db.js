46ac147e783eae22b83fd3f5d14011d13ebd1472
aswinning <aswinrox03@gmail.com>
2025-10-21 21:38:59 +0530

    db error message fixed

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,8 +5,9 @@ mongoose.connect("mongodb://localhost:27017/My-portal");
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongoDB");
 });
-mongoose.connection.on("error", () => {
-  console.log(`mongoDB connection error : ${error}`);
+
+mongoose.connection.on("error", (err) => {
+  console.log(`mongoDB connection error: ${err.message}`);
 });
 
 module.exports = mongoose;
